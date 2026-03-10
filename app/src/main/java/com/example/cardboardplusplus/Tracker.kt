027cd62af4eaaa690d863ea23e8d2bd488e5928b
Gabriel Cordeiro Bosse <gabrielbosse25@gmail.com>
2026-03-09 22:24:39 -0300

    Improve 6DoF Accuracy (Orientation + Accel).

--- app/src/main/java/com/example/cardboardplusplus/Tracker.kt
+++ app/src/main/java/com/example/cardboardplusplus/Tracker.kt
@@ -6,6 +6,8 @@ import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import kotlin.math.abs
+import kotlin.math.atan2
+import kotlin.math.sqrt
 
 object Tracker : SensorEventListener {
     private var sensorManager: SensorManager? = null
@@ -24,6 +26,10 @@ object Tracker : SensorEventListener {
 
     var position = FloatArray(3)
         private set
+
+    private val rotationMatrix = FloatArray(9)
+    private val orientationAngles = FloatArray(3)
+    private val accelWorld = FloatArray(3)
 
     private var lastTimestamp: Long = 0
 
@@ -70,9 +76,12 @@ object Tracker : SensorEventListener {
                 gyroscope[2] = event.values[2]
             }
             Sensor.TYPE_ROTATION_VECTOR -> {
-                rotation[0] = event.values[0]
-                rotation[1] = event.values[1]
-                rotation[2] = event.values[2]
+                SensorManager.getRotationMatrixFromVector(rotationMatrix, event.values)
+                SensorManager.getOrientation(rotationMatrix, orientationAngles)
+
+                rotation[0] = Math.toDegrees(orientationAngles[1].toDouble()).toFloat()
+                rotation[1] = Math.toDegrees(orientationAngles[2].toDouble()).toFloat()
+                rotation[2] = Math.toDegrees(orientationAngles[0].toDouble()).toFloat()
             }
             Sensor.TYPE_LINEAR_ACCELERATION -> {
                 for (i in event.values.indices) {
@@ -83,9 +92,20 @@ object Tracker : SensorEventListener {
                     }
                 }
 
-                position[0] += accelerometer[0] * dt
-                position[1] += accelerometer[1] * dt
-                position[2] += accelerometer[2] * dt
+                val rotationVecAvailable = rotationMatrix.any { it != 0f }
+                if (rotationVecAvailable) {
+                    accelWorld[0] = rotationMatrix[0] * accelerometer[0] + rotationMatrix[1] * accelerometer[1] + rotationMatrix[2] * accelerometer[2]
+                    accelWorld[1] = rotationMatrix[3] * accelerometer[0] + rotationMatrix[4] * accelerometer[1] + rotationMatrix[5] * accelerometer[2]
+                    accelWorld[2] = rotationMatrix[6] * accelerometer[0] + rotationMatrix[7] * accelerometer[1] + rotationMatrix[8] * accelerometer[2]
+
+                    position[0] += accelWorld[0] * dt
+                    position[1] += accelWorld[1] * dt
+                    position[2] += accelWorld[2] * dt
+                } else {
+                    position[0] += accelerometer[0] * dt
+                    position[1] += accelerometer[1] * dt
+                    position[2] += accelerometer[2] * dt
+                }
             }
         }
     }
